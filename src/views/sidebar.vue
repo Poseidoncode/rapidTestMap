0bf1e8810c486cf63adaf6f4c8f0e8c418b5fbf1
Poseidoncode <66759971+Poseidoncode@users.noreply.github.com>
2022-04-18 01:46:57 +0800

    sidebar

--- src/views/sidebar.vue
+++ src/views/sidebar.vue
@@ -30,180 +30,46 @@
       </button>
     </div>
     <hr class="my-3" />
-    <div class="flex justify-between mt-6">
-      <div class="flex">
-        <img
-          class="h-20 w-20 object-cover rounded"
-          src="https://images.unsplash.com/photo-1593642632823-8f785ba67e45?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80"
-          alt=""
-        />
-        <div class="mx-3">
-          <h3 class="text-sm text-gray-600">Mac Book Pro</h3>
-          <div class="flex items-center mt-2">
-            <button
-              class="text-gray-500 focus:outline-none focus:text-gray-600"
-            >
-              <svg
-                class="h-5 w-5"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                ></path>
-              </svg>
-            </button>
-            <span class="text-gray-700 mx-2">2</span>
-            <button
-              class="text-gray-500 focus:outline-none focus:text-gray-600"
-            >
-              <svg
-                class="h-5 w-5"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-              </svg>
-            </button>
-          </div>
-        </div>
-      </div>
-      <span class="text-gray-600">20$</span>
-    </div>
-    <div class="flex justify-between mt-6">
-      <div class="flex">
-        <img
-          class="h-20 w-20 object-cover rounded"
-          src="https://images.unsplash.com/photo-1593642632823-8f785ba67e45?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80"
-          alt=""
-        />
-        <div class="mx-3">
-          <h3 class="text-sm text-gray-600">Mac Book Pro</h3>
-          <div class="flex items-center mt-2">
-            <button
-              class="text-gray-500 focus:outline-none focus:text-gray-600"
-            >
-              <svg
-                class="h-5 w-5"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                ></path>
-              </svg>
-            </button>
-            <span class="text-gray-700 mx-2">2</span>
-            <button
-              class="text-gray-500 focus:outline-none focus:text-gray-600"
-            >
-              <svg
-                class="h-5 w-5"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-              </svg>
-            </button>
-          </div>
-        </div>
-      </div>
-      <span class="text-gray-600">20$</span>
-    </div>
-    <div class="flex justify-between mt-6">
-      <div class="flex">
-        <img
-          class="h-20 w-20 object-cover rounded"
-          src="https://images.unsplash.com/photo-1593642632823-8f785ba67e45?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80"
-          alt=""
-        />
-        <div class="mx-3">
-          <h3 class="text-sm text-gray-600">Mac Book Pro</h3>
-          <div class="flex items-center mt-2">
-            <button
-              class="text-gray-500 focus:outline-none focus:text-gray-600"
-            >
-              <svg
-                class="h-5 w-5"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                ></path>
-              </svg>
-            </button>
-            <span class="text-gray-700 mx-2">2</span>
-            <button
-              class="text-gray-500 focus:outline-none focus:text-gray-600"
-            >
-              <svg
-                class="h-5 w-5"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-              </svg>
-            </button>
-          </div>
-        </div>
-      </div>
-      <span class="text-gray-600">20$</span>
-    </div>
-    <div class="mt-8">
-      <form class="flex items-center justify-center">
-        <input
-          class="form-input w-48"
-          type="text"
-          placeholder="Add promocode"
-        />
-        <button
-          class="ml-3 flex items-center px-3 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500"
+
+    <div v-for="(item, i) in 50" :key="`headers${i}`" class="mt-2">
+      <div class="max-w-2xl mx-auto">
+        <div
+          class="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700"
         >
-          <span>Apply</span>
-        </button>
-      </form>
+          <div class="p-5">
+            <a href="#">
+              <h5
+                class="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white"
+              >
+                Noteworthy technology acquisitions 2021
+              </h5>
+            </a>
+            <p class="font-normal text-gray-700 mb-3 dark:text-gray-400">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+            <a
+              href="#"
+              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Read more
+              <svg
+                class="-mr-1 ml-2 h-4 w-4"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
-    <a
-      class="flex items-center justify-center mt-4 px-3 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500"
-    >
-      <span>Chechout</span>
-      <svg
-        class="h-5 w-5 mx-2"
-        fill="none"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path d="M17 8l4 4m0 0l-4 4m4-4H3"></path>
-      </svg>
-    </a>
   </div>
 </template>
 
@@ -238,7 +104,7 @@ export default {
   cursor: pointer;
   color: #fff;
   height: 36px;
-  background: #f1cd33;
+  background: #1c54d4;
   border-radius: 50%;
 }
 </style>
