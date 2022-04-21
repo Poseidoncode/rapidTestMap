f4353f18180290e6302fb19fbab4add40240f931
Poseidoncode <posidomhu@gmail.com>
2022-04-21 17:38:06 +0800

    fix-css

--- src/views/sidebar.vue
+++ src/views/sidebar.vue
@@ -87,7 +87,7 @@
               @click.prevent="setCenter(item)"
             >
               Map
-              <svg
+              <!-- <svg
                 class="-mr-1 ml-2 h-4 w-4"
                 fill="currentColor"
                 viewBox="0 0 20 20"
@@ -98,17 +98,17 @@
                   d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
                   clip-rule="evenodd"
                 ></path>
-              </svg>
+              </svg> -->
             </a>
 
             <a
               href="#"
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ml-3"
-              @click.prevent="setDestination(item)"
+              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ml-2"
+              @click.prevent="searchDestination(item)"
             >
-              Google導航
+              搜尋診所
               <svg
-                class="-mr-1 ml-2 h-4 w-4"
+                class="-mr-1 ml-2 h-4 w-4 hide-item"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -122,10 +122,11 @@
             </a>
             <a
               href="#"
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ml-3"
-              @click.prevent="searchDestination(item)"
+              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ml-2"
+              @click.prevent="setDestination(item)"
             >
-              診所搜尋
+              <span class="hide-item">Google</span>
+              導航
               <svg
                 class="-mr-1 ml-2 h-4 w-4"
                 fill="currentColor"
@@ -405,5 +406,14 @@ export default defineComponent({
   color: #fff;
   background: #1c54d4;
   width: 97px;
+}
+
+.hide-item {
+  display: block;
+}
+@media screen and (max-width: 500px) {
+  .hide-item {
+    display: none !important;
+  }
 }
 </style>
